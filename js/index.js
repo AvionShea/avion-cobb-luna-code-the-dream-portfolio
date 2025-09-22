e4c328f4ddb7ae2a8b36ba42e6ec2bcf0d280438
Avion C. <83898002+AvionShea@users.noreply.github.com>
2025-09-22 00:04:14 -0400

    Refactor CSS styles for about section and update JavaScript comments for clarity

--- js/index.js
+++ js/index.js
@@ -7,24 +7,24 @@ const today = new Date();
 const thisYear = today.getFullYear();
 
 //---------------FOOTER CREATION---------------
-// Create new footer element
+// Create new <footer> element
 const footerElement = document.createElement("footer");
 
-// Append footer to the body
+// Append <footer> to the body
 body.appendChild(footerElement);
 const footer = document.querySelector("footer");
 
 //-------------COPYRIGHT CREATION---------------
-//Create new copyright paragraph element
+//Create new copyright <p> element
 const copyright = document.createElement("p");
 
 //Added Creator Name, Copyright symbol, and Current Year to HTML
-copyright.innerHTML = `${"Copyright"} &copy;${thisYear} ${creatorName}`;
+copyright.innerHTML = `Copyright &copy;${thisYear} ${creatorName}. All rights reserved.`;
 // Changed copyright text to white
 copyright.style.color = "white";
 // Changed copyright text size
 copyright.style.fontSize = "1.5rem";
-// Added copyright info to footer element
+// Added copyright info to <footer> element
 footer.appendChild(copyright);
 
 //--------------SKILLS LIST CREATION-------------
@@ -32,7 +32,7 @@ footer.appendChild(copyright);
 const skillSection = document.getElementById("skills");
 
 //--------HARD SKILLS----------
-// Grabbed hard skills UL from HTML
+// Grabbed hard skills <ul> from HTML
 const hardSkillsList = skillSection.querySelector(".hard-skills-list");
 const hardSkills = [
     "JavaScript",
@@ -48,7 +48,7 @@ const hardSkills = [
     "Agile Methodologies"
 ];
 
-//For loop to create li for each element in hardSkills array and place in HTML
+//For loop to create <li> for each element in hardSkills [array] and place in HTML
 for (let i = 0; i < hardSkills.length; i++) {
     let skill = document.createElement("li");
     skill.innerText = hardSkills[i];
@@ -56,7 +56,7 @@ for (let i = 0; i < hardSkills.length; i++) {
 };
 
 // ---------SOFT SKILLS------------
-// Grabbed soft skills UL from HTML
+// Grabbed soft skills <ul> from HTML
 const softSkillsList = skillSection.querySelector(".soft-skills-list");
 const softSkill = [
     "Problem Solving",
@@ -72,7 +72,7 @@ const softSkill = [
     "Critical Thinking"
 ];
 
-//For loop to create li for each element in softSkills array and place in HTML
+//For loop to create <li> for each element in softSkills [array] and place in HTML
 for (let i = 0; i < softSkill.length; i++) {
     let skill = document.createElement("li");
     skill.innerText = softSkill[i];
